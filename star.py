302c4b8bbdce19ab35514ab05bdd057d832e32ef
Ronel <ronel.kpossou@epitech.eu>
2023-08-23 09:52:28 +0100

    Add star.py

---
+++ star.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def getArguments():
+    if len(sys.argv) == 1:
+        print("Usage: ./star")
+        sys.exit()
+    else:
+        return sys.argv[1]
+
+
+if __name__ == "__main__":
+    size = int(getArguments())
+
+    if size == 0:
+        exit()
+    else:
+        rows = size * 4 + 1
+        cols = size * 6 + 1
+        board = [[" " for i in range(cols)] for j in range(rows)]
+
+        middle_row = int(rows / 2)
+        middle_col = int(cols / 2)
+
+        board[0][middle_col] = "*"
+
+        first_row_pointer = middle_col - 1
+        last_row_pointer = middle_col + 1
+
+        row = 1
+
+        for i in range(size - 1):
+            board[row][first_row_pointer] = "*"
+            board[row][last_row_pointer] = "*"
+            first_row_pointer -= 1
+            last_row_pointer += 1
+            row += 1
+
+        while first_row_pointer != -1:
+            board[row][first_row_pointer] = "*"
+            board[row][last_row_pointer] = "*"
+            first_row_pointer -= 1
+            last_row_pointer += 1
+
+        first_row_pointer += 2
+        last_row_pointer -= 2
+        row += 1
+
+        for i in range(size):
+            board[row][first_row_pointer] = "*"
+            board[row][last_row_pointer] = "*"
+            first_row_pointer += 1
+            last_row_pointer -= 1
+            row += 1
+
+        for i in range(middle_row):
+            board[rows - i - 1] = board[i]
+
+        for row in board:
+            print("".join(row))
